d44e33f6ba6b29045634a6b6dec4141fae16397b
Joanna <m.joanna.foss@gmail.com>
2021-11-16 16:49:48 -0600

    feat: explored insert values into table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,15 @@
 USE codeup_test_db;
 
+-- this is a comment. Statements are not case sensitive ACTUALLY.
 CREATE TABLE IF NOT EXISTS albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     artist VARCHAR(50) DEFAULT 'NONE' NOT NULL,
     name VARCHAR(50) NOT NULL,
-    release_date INT NOT NULL,
+    release_date DATE NOT NULL,
     sales float NOT NULL,
-    genre VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    genre VARCHAR(50) NOT NULL
 );
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Lady Gaga', 'The Fame', 2008, 4900000.00, 'pop'),
+    ('Mystery Artist', 'Mystery Album', 2021, 1.11, 'punk');
