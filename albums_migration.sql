64b570a239a9bd47b8ea0de430dd0c523c66c7d9
Joanna <m.joanna.foss@gmail.com>
2021-11-17 10:32:11 -0600

    feat: finished review with larry

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,13 +3,13 @@ USE codeup_test_db;
 -- this is a comment. Statements are not case sensitive ACTUALLY.
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE' NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales float NOT NULL,
-    genre VARCHAR(50) NOT NULL
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name_of_album VARCHAR(100) NOT NULL,
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR(50)
 );
 
-INSERT INTO albums (artist, name, release_date, sales, genre)
+INSERT INTO albums (artist, name_of_album, release_date, sales, genre)
 VALUES ('Lady Gaga', 'The Fame', 2008, 4900000.00, 'pop'),
     ('Mystery Artist', 'Mystery Album', 2021, 1.11, 'punk');
