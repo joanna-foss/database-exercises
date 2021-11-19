b30bbd11ad5b4382a2038805407f66f1254d9074
Joanna <m.joanna.foss@gmail.com>
2021-11-19 13:32:05 -0600

    feat: completed group by

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -14,3 +14,19 @@ from employees
 where (last_name like '%e'
 and last_name like 'e%')
 group by last_name;
+
+select distinct last_name from employees
+where (last_name
+like '%q%'
+and last_name
+not like '%qu%');
+
+select count(gender)
+as 'how many',
+gender
+as 'which gender'
+from employees
+where (first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya')
+group by gender;
