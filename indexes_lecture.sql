91ba358c3d516ed18c726ec4ac4e9d338152db0b
Joanna <m.joanna.foss@gmail.com>
2021-11-22 10:15:15 -0600

    feat: indexes/unique keys w douglas

--- indexes_lecture.sql
+++ indexes_lecture.sql
@@ -5,11 +5,11 @@ USE quasar_example_db;
 DROP TABLE IF EXISTS pets;
 
 CREATE TABLE pets (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     pet_name VARCHAR(30) NOT NULL,
     owner_name VARCHAR(30),
     age INT,
-    PRIMARY KEY (id)
+    UNIQUE unique_owner_name (owner_name)
 );
 
 INSERT INTO pets (pet_name, owner_name, age) VALUES
@@ -22,3 +22,16 @@ SELECT * FROM pets;
 SELECT * FROM pets WHERE id = 3;
 
 EXPLAIN SELECT id FROM pets WHERE id = 3;
+EXPLAIN SELECT id FROM pets WHERE owner_name = 'Cathy Smith';
+
+#show indexes
+SHOW INDEX FROM pets;
+
+SELECT * from pets;
+
+DESCRIBE pets;
+
+INSERT INTO pets (pet_name, owner_name, age) VALUE
+    ('Trixie', 'Cathy Smith', 1); #CANNOT ADD ANOTHER CATHY SMITH. Unique key on owner_name prevents this.
+INSERT INTO pets (pet_name, owner_name, age) VALUE
+    ('Trixie', 'Bobby Smith', 1);
